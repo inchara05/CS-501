6ef1cb52329341f0d24cd1fa7827171773e157d3
Inchara N <49956552+inchara05@users.noreply.github.com>
2022-10-18 02:12:36 -0400

    Create Edition12C1E1_5_Expressions.java

---
+++ Assignment 1/Edition12C1E1_5_Expressions.java
@@ -0,0 +1,19 @@
+//user defined package
+//package com.packages;
+
+/* The Edition12C1E1_5_Expressions is a class to compute
+ * the expression given and 
+ * display the results on the console.
+ */
+public class Edition12C1E1_5_Expressions 
+{
+	//main method
+	public static void main(String[] args)
+	{
+		System.out.println("Computing the result of the given Expressions:\n");
+		System.out.print("The result of ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5) is ");
+			
+		//Displays the result of the given expressions on the console.
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}
