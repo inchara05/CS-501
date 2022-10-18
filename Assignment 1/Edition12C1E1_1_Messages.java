7c9cbb29709f3683e2cbb414b0eda35db23a2c4d
Inchara N <49956552+inchara05@users.noreply.github.com>
2022-10-18 02:11:49 -0400

    Update Edition12C1E1_1_Messages.java

--- Assignment 1/Edition12C1E1_1_Messages.java
+++ Assignment 1/Edition12C1E1_1_Messages.java
@@ -1,1 +1,19 @@
+//user defined package
+//package com.packages;
+
+/* The class Edition12C1E1_1_Messages is created to print 
+ * the three messages on the console.
+ */
+public class Edition12C1E1_1_Messages 
+{
+	//main method
+	public static void main(String[] args)
+	{	
+		//Displays three messages on the console
+		System.out.println("Displaying three Messages:\n");
+		System.out.println("1) Welcome to Java");
+		System.out.println("2) Welcome to Computer Science");
+		System.out.println("3) Programming is fun");
+	}
+}
 
